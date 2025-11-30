996344c1ab69e44361ea40606ac654785639fb86
xNewz <pargornruasijan@gmail.com>
2025-11-30 14:52:34 +0700

    remove: owner eval

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,6 @@ import Library from "./pages/Library";
 import ManageSoldier from "./pages/ManageSoldier";
 import Message from "./pages/Message";
 import SoldierDashboard from "./pages/SoldierDashboard";
-import OwnerEvaluation from "./pages/OwnerEvaluation";
 
 const normalizeRole = (role = "") => role.toUpperCase();
 
@@ -169,14 +168,6 @@ const router = createBrowserRouter([
                 element: (
                     <ProtectedRoute allowedRoles={["ADMIN", "OWNER"]}>
                         <SoldierDashboard />
-                    </ProtectedRoute>
-                )
-            },
-            {
-                path: "owner-evaluation",
-                element: (
-                    <ProtectedRoute allowedRoles={["ADMIN", "OWNER", "SUB_ADMIN"]}>
-                        <OwnerEvaluation />
                     </ProtectedRoute>
                 )
             },
